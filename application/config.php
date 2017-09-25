d63f0ee680bcfef223c59b4a655206f90d6bf9e4
Lxx <779219930@qq.com>
2017-09-25 15:06:04 +0800

    v1.0 之图红网初始化项目框架

---
+++ application/config.php
@@ -0,0 +1,41 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+return [
+    'url_route_on' => true,
+//    'log'          => [
+//        'type' => 'trace', // 支持 socket trace file
+//    ],
+
+    /* 默认模块和控制器 */
+    'default_module' => 'home',
+
+    /* URL配置 */
+    'base_url'=>'',
+    'parse_str'=>[
+        '__ROOT__' => '/',
+        '__STATIC__' => '/static',
+        '__ADMIN__' => '/admin',
+        '__HOME__' => '/home',
+    ],
+
+    /* UC用户中心配置 */
+    'uc_auth_key' => '(.t!)=JTb_OPCkrD:-i"QEz6KLGq5glnf^[{p;je',
+
+    /* 分页自定义 */
+    'paginate' => [
+        'type'     => '\org\Pager',
+        'var_page' => 'page',
+        'list_rows'=> 12
+    ],
+   
+];
---
+++ application/home/controller/Index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lxx<779219930@qq.com>
+ * Date: 2016/4/20
+ * Time: 13:47
+ */
+
+namespace app\home\controller;
+use app\home\model\Message;
+use app\home\model\WechatUser;
+use com\wechat\TPQYWechat;
+use think\Config;
+use think\Controller;
+use app\user\controller\Index as APIIndex;
+use think\Log;
+
+/**
+ * 党建主页
+ */
+class Index extends Controller {
+    public function index(){
+
+        return $this->fetch();
+    }
+}
