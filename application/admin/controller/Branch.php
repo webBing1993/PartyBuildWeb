997d8df9526a36fec0f7bf0111d330068364cea6
Lxx <779219930@qq.com>
2017-09-26 15:20:07 +0800

    v1.0支部建设后台管理

---
+++ application/admin/controller/Branch.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lxx<779219930@qq.com>
+ * Date: 2017/9/26
+ * Time: 14:53
+ */
+
+namespace app\admin\controller;
+
+use app\admin\model\Branch as BranchModel;
+/**
+ * Class Branch
+ * @package app\admin\controller
+ * 支部建设
+ */
+class Branch extends Admin {
+    /**
+     * 主页列表
+     */
+    public function index() {
+        $map = array(
+            'status' => array('eq',1),  // 推送  未推送
+        );
+        $list = $this->lists('Branch',$map);
+        int_to_string($list,array(
+            'status' => array(0=>"未审核",1=>'已发布'),
+            'type' => array(1=>"通知公告",2=>"活动报道",3=>"支部内刊")
+        ));
+        $this->assign('list',$list);
+
+        return $this->fetch();
+    }
+
+    /**
+     * 新增
+     */
+    public function add() {
+        if(IS_POST) {
+            $data = input('post.');
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if(empty($data['id'])) {
+                unset($data['id']);
+            }
+            $Model = new BranchModel();
+            $info = $Model->validate(true)->save($data);
+            if($info) {
+                return $this->success("新增成功",Url('Branch/index'));
+            }else{
+                return $this->get_update_error_msg($Model->getError());
+            }
+        }else{
+            $this->default_pic();
+            $this->assign('msg','');
+
+            return $this->fetch('edit');
+        }
+    }
+
+    /**
+     * 修改
+     */
+    public function edit() {
+        if(IS_POST) {
+            $data = input('post.');
+            $Model = new BranchModel();
+            $info = $Model->validate(true)->save($data,['id'=>input('id')]);
+            if($info){
+                return $this->success("修改成功",Url("Branch/index"));
+            }else{
+                return $this->get_update_error_msg($Model->getError());
+            }
+        }else{
+            $this->default_pic();
+            $id = input('id');
+            $msg = BranchModel::get($id);
+            $this->assign('msg',$msg);
+
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 删除
+     */
+    public function del() {
+        $id = input('id');
+        $data['status'] = '-1';
+        $info = BranchModel::where('id',$id)->update($data);
+        if($info) {
+            return $this->success("删除成功");
+        }else{
+            return $this->error("删除失败");
+        }
+    }
+}
---
+++ application/admin/validate/Branch.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lxx<779219930@qq.com>
+ * Date: 2017/9/26
+ * Time: 14:54
+ */
+
+namespace app\admin\validate;
+
+
+use think\Validate;
+
+class Branch extends Validate {
+    protected $rule = [
+        'front_cover' => 'require',
+        'type' => 'require',
+        'title' => 'require',
+        'content' => 'require',
+        'publisher' => 'require',
+    ];
+
+    protected $message = [
+        'front_cover' => '封面图片不能为空',
+        'type' => '类型不能为空',
+        'title' =>  '标题不能为空',
+        'content'  =>  '内容不能为空',
+        'publisher' => '发布者不能为空',
+    ];
+}
